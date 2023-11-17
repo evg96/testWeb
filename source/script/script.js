d39813d9db4978c0819f689075b8375b3ca0a001
evg96 <joinuniq1665@mail.ru>
2023-11-18 01:09:27 +0300

    commit

--- source/script/script.js
+++ source/script/script.js
@@ -5,11 +5,23 @@ import { expandAccord, listEvent } from './elements/accordeon.js';
 
 let tg = window.Telegram.WebApp;
 
-// let event = document.getElementById('test_event');
+tg.expand(); //расширяем на все окно  
 
-tg.MainButton.text = 'test';
-tg.MainButton.isVisible = true;
-tg.MainButton.enable();
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
+
+
+btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	if (tg.MainButton.isVisible){ //если кнопка показана 
+		tg.MainButton.hide() //скрываем кнопку 
+	}
+  else{ //иначе
+  	tg.MainButton.show() //показываем 
+  }
+});
 
 await fetch('https://telegram-client-services.ru/salon/Tsirulnik-Khimki-1')
 .then((response) => response.json())
