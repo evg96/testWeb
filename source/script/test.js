7c60cc873acec8a289d47245580e1cf1e7a905ae
evg96 <joinuniq1665@mail.ru>
2023-11-18 01:47:59 +0300

    commit

--- source/script/test.js
+++ source/script/test.js
@@ -1,9 +1,9 @@
 let tg = window.Telegram.WebApp;
 
-// tg.expand(); //расширяем на все окно  
+tg.expand(); //расширяем на все окно  
 
 tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.setText(tg.initDataUnsafe.user.id); //изменяем текст кнопки иначе
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
