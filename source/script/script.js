ac6b2c8fc70cad74ca042a166b1642b5874a0807
evg96 <joinuniq1665@mail.ru>
2023-11-18 01:40:03 +0300

    commit

--- source/script/script.js
+++ source/script/script.js
@@ -1,29 +1,6 @@
 import { displayServicesGroups, displayServices } from './services.js';
 import { expandAccord, listEvent } from './elements/accordeon.js';
 
-
-
-let tg = window.Telegram.WebApp;
-
-tg.expand(); //расширяем на все окно  
-
-tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
-tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
-
-const masters = document.getElementById('btn_1')
-masters.addEventListener('click', function(){ 
-    // console.log('btn');
-	if (tg.MainButton.isVisible){ //если кнопка показана 
-		tg.MainButton.hide() //скрываем кнопку 
-	}
-  else{ //иначе
-  	tg.MainButton.show() //показываем 
-  }
-});
-
 await fetch('https://telegram-client-services.ru/salon/Tsirulnik-Khimki-1')
 .then((response) => response.json())
 .then((datas) => {
---
+++ source/script/test.js
@@ -0,0 +1,20 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand(); //расширяем на все окно  
+
+tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
+tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
+tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
+tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
+tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры 
+
+const masters = document.getElementById('btn_1')
+masters.addEventListener('click', function(){ 
+    // console.log('btn');
+	if (tg.MainButton.isVisible){ //если кнопка показана 
+		tg.MainButton.hide() //скрываем кнопку 
+	}
+  else{ //иначе
+  	tg.MainButton.show() //показываем 
+  }
+});
