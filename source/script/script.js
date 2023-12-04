0bf9f7a5ddfa3438ba30dc36fe29a610d74168a6
evg96 <joinuniq1665@mail.ru>
2023-12-04 22:01:26 +0300

    commit

--- source/script/script.js
+++ source/script/script.js
@@ -6,8 +6,20 @@ import { initTG } from './telegram.js';
 const slectService = document.querySelector('#btn-service')
 const slectProvider = document.querySelector('#btn-provider')
 
-// slectService.addEventListener('click', function(){
-//     genServiceList();
+slectService.addEventListener('click', function(){
+    genServiceList();
+    let tg = initTG();
+    tg.expand();
+    tg.BackButton.show();
+    Telegram.WebApp.onEvent('backButtonClicked', function(){
+        location.reload();
+    });   
+});
+
+// servicesListener()
+
+// function servicesListener(){
+//     genServiceList()
 //     let tg = initTG();
 //     tg.expand();
 //     tg.BackButton.show();
@@ -25,58 +37,35 @@ const slectProvider = document.querySelector('#btn-provider')
 //         main.innerHTML = content;
 //         servicesListener();
 //     });   
-// });
-
-servicesListener()
-
-function servicesListener(){
-    genServiceList()
-    let tg = initTG();
-    tg.expand();
-    tg.BackButton.show();
-    Telegram.WebApp.onEvent('backButtonClicked', function(){
-        const main = document.querySelector('main');
-        const content = `<div class="main_select">
-                            <div id="btn-service" class="btn" role="button">
-                                Выбрать услугу
-                            </div>
-                            <div id="btn-provider" class="btn" role="button">
-                                Выбрать мастера
-                            </div>
-                        </div>`
-        console.log('clic back');
-        main.innerHTML = content;
-        servicesListener();
-    });   
-}
+// }
 
 
-function genServiceList(){
-    slectService.addEventListener('click', function(){
-    let main = document.querySelector('main');
-    main.innerHTML = `<div class="srv-container">
-    <div id="service-list">
-        <div class="accordion" id="accordion"></div></div>
-    </div>`;
-    fetch('./app')
-    .then((response) => response.json())
-    .then((datas) => {
-        displayServicesGroups(datas);
-        let ids = '';
-        for (let data of datas){
-            ids += `${data.id},`
-        }
-        ids = ids.substring(0, ids.length - 1);  // delete last comma
-        fetch(`./app/service?servgroup=${ids}`)
-        .then((respons)=> respons.json())
-        .then((services) => {
-            displayServices(services);
-            expandAccord();
-            listEvent();
-        })
-    })
-});
-}
+// function genServiceList(){
+//     slectService.addEventListener('click', function(){
+//         let main = document.querySelector('main');
+//         main.innerHTML = `<div class="srv-container">
+//         <div id="service-list">
+//             <div class="accordion" id="accordion"></div></div>
+//         </div>`;
+//         fetch('./app')
+//         .then((response) => response.json())
+//         .then((datas) => {
+//             displayServicesGroups(datas);
+//             let ids = '';
+//             for (let data of datas){
+//                 ids += `${data.id},`
+//             }
+//             ids = ids.substring(0, ids.length - 1);  // delete last comma
+//             fetch(`./app/service?servgroup=${ids}`)
+//             .then((respons)=> respons.json())
+//             .then((services) => {
+//                 displayServices(services);
+//                 expandAccord();
+//                 listEvent();
+//             })
+//         })
+//     });
+// }
 
 slectProvider.addEventListener('click', function(){
     let main = document.querySelector('main');
