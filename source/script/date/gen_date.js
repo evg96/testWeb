552803fa31886d2b1ceec8e51b419f8c93941da9
evg96 <joinuniq1665@mail.ru>
2024-01-06 00:47:43 +0300

    send data

--- source/script/date/gen_date.js
+++ source/script/date/gen_date.js
@@ -1,9 +1,9 @@
 export class CtsDate{
     constructor(){
         const date = new Date();
-        console.log("UTC", date.toUTCString());
-        console.log("UTC2", date.getTime());
-        this.date = new Date(date.getUTCTime() + (5*60*1000));
+        console.log("timezone", date.getHours());
+        console.log("UTC", date.getUTCHours());
+        this.date = new Date(date.getTime() + (5*60*1000));
         // this.date = new Date();
     }
     getWeek(){
