a14050ee0f6979d86241301540461eee29ffcdd2
evg96 <joinuniq1665@mail.ru>
2024-01-08 23:58:08 +0300

    send data

--- source/script/info_page.js
+++ source/script/info_page.js
@@ -5,6 +5,7 @@ import { tg } from './telegram.js'
 closeApp()
 
 function closeApp(){
+    console.log("here");
     tg.MainButton.text = "Закрыть";
     tg.MainButton.color = "#3390ec";
     tg.MainButton.isVisible = true;
