669158b324ab0b0c1d24ffc8e00d237efc81d7a5
evg96 <joinuniq1665@mail.ru>
2024-01-09 02:37:23 +0300

    send data

--- source/script/calendar_page.js
+++ source/script/calendar_page.js
@@ -98,7 +98,7 @@ function showTime(orderInfo){
             let welMes = document.querySelector('[class=choose_day]');
             welMes.setAttribute('style', 'display: none');
             orderInfo.date = new Date(date);
-            dDescr.removeAttribute('style', 'visibility');
+            dDescr.setAttribute('style', 'visibility: visible');
             dDescr.innerHTML = `${orderInfo.getDay()} ${orderInfo.getMounth()}`;
             // console.log("mounth", orderInfo.getDay(), orderInfo.getMounth());
             let totalDur = 0;
