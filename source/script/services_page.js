cdd36aa341520867ae5223e75240a56c55a5fc35
evg96 <joinuniq1665@mail.ru>
2024-01-18 21:33:56 +0300

    send data

--- source/script/services_page.js
+++ source/script/services_page.js
@@ -39,84 +39,22 @@ function expandList(){
     }
 }
 
-function listServices(){
-    let coll = document.getElementsByClassName('information');
-    for(let i = 0; i < coll.length; i++){
-        const checkbox = coll[i].querySelector('[class=real-checkbox]');
-        checkbox.addEventListener('change', function() {
-            const id = coll[i].querySelector('[class=description]').dataset.id;
-            const priceSelector = coll[i].querySelector('[class=price]');
-            const duration = coll[i].querySelector('[class=duration]').dataset.duration;
-            const title = coll[i].querySelector('h3').innerText;
-            const price = getPrice(priceSelector.innerHTML);
-            if (checkbox.checked){
-                orderInfo.addService(id, title, price, duration);
-                console.log("checkbox checked", id);
-            }else{
-                orderInfo.deleteService(id);
-                console.log("checkbox not checked", id);
-            }
-            if (orderInfo.getNumberOfServices() > 0){
-                tg.MainButton.text = `Выбрано услуг: ${orderInfo.getNumberOfServices()}                  Цена: ${orderInfo.getFullPrice()} ₽`;
-                tg.MainButton.color = "#3390ec";
-                tg.MainButton.isVisible = true;
-                tg.MainButton.show();
-                showEmployee();
-            }else{
-                tg.MainButton.hide();
-            }
-        })
-    }
-
-    // for(let i = 0; i < coll.length; i++){
-    //     if (checkbox.checked){
-    //         console.log(getPrice(priceSelector.innerHTML))
-    //     }
-    // }
-
-    //     console.log(coll[i].querySelector('[class=real-checkbox]'));
-
-        // coll[i].addEventListener('click', function(){
-            // const id = coll[i].querySelector('[class=description]').dataset.id;
-            // const checkbox = coll[i].querySelector('[class=real-checkbox]');
-            // const priceSelector = coll[i].querySelector('[class=price]');
-            // const duration = coll[i].querySelector('[class=duration]').dataset.duration;
-            // const title = coll[i].querySelector('h3').innerText;
-            // checkbox.checked == true ? checkbox.checked = false : checkbox.checked = true;
-            // const price = getPrice(priceSelector.innerHTML);
-            // if (checkbox.checked){
-            //     orderInfo.addService(id, title, price, duration)
-            // }else{
-            //     orderInfo.deleteService(id)
-            // }
-            // if (orderInfo.getNumberOfServices() > 0){
-            //     tg.MainButton.text = `Выбрано услуг: ${orderInfo.getNumberOfServices()}                  Цена: ${orderInfo.getFullPrice()} ₽`;
-            //     tg.MainButton.color = "#3390ec";
-            //     tg.MainButton.isVisible = true;
-            //     tg.MainButton.show();
-            //     showEmployee();
-            // }else{
-            //     tg.MainButton.hide();
-            // }
-        // })
-    // }
-}
-
 // function listServices(){
 //     let coll = document.getElementsByClassName('information');
 //     for(let i = 0; i < coll.length; i++){
-//         coll[i].addEventListener('click', function(){
+//         const checkbox = coll[i].querySelector('[class=real-checkbox]');
+//         checkbox.addEventListener('change', function() {
 //             const id = coll[i].querySelector('[class=description]').dataset.id;
-//             const checkbox = coll[i].querySelector('[class=real-checkbox]');
 //             const priceSelector = coll[i].querySelector('[class=price]');
 //             const duration = coll[i].querySelector('[class=duration]').dataset.duration;
 //             const title = coll[i].querySelector('h3').innerText;
-//             checkbox.checked == true ? checkbox.checked = false : checkbox.checked = true;
 //             const price = getPrice(priceSelector.innerHTML);
 //             if (checkbox.checked){
-//                 orderInfo.addService(id, title, price, duration)
+//                 orderInfo.addService(id, title, price, duration);
+//                 console.log("checkbox checked", id);
 //             }else{
-//                 orderInfo.deleteService(id)
+//                 orderInfo.deleteService(id);
+//                 console.log("checkbox not checked", id);
 //             }
 //             if (orderInfo.getNumberOfServices() > 0){
 //                 tg.MainButton.text = `Выбрано услуг: ${orderInfo.getNumberOfServices()}                  Цена: ${orderInfo.getFullPrice()} ₽`;
@@ -130,6 +68,35 @@ function listServices(){
 //         })
 //     }
 // }
+
+function listServices(){
+    let coll = document.getElementsByClassName('information');
+    for(let i = 0; i < coll.length; i++){
+        coll[i].addEventListener('click', function(){
+            const id = coll[i].querySelector('[class=description]').dataset.id;
+            const checkbox = coll[i].querySelector('[class=real-checkbox]');
+            const priceSelector = coll[i].querySelector('[class=price]');
+            const duration = coll[i].querySelector('[class=duration]').dataset.duration;
+            const title = coll[i].querySelector('h3').innerText;
+            checkbox.checked == true ? checkbox.checked = false : checkbox.checked = true;
+            const price = getPrice(priceSelector.innerHTML);
+            if (checkbox.checked){
+                orderInfo.addService(id, title, price, duration)
+            }else{
+                orderInfo.deleteService(id)
+            }
+            if (orderInfo.getNumberOfServices() > 0){
+                tg.MainButton.text = `Выбрано услуг: ${orderInfo.getNumberOfServices()}                  Цена: ${orderInfo.getFullPrice()} ₽`;
+                tg.MainButton.color = "#3390ec";
+                tg.MainButton.isVisible = true;
+                tg.MainButton.show();
+                showEmployee();
+            }else{
+                tg.MainButton.hide();
+            }
+        })
+    }
+}
 
 function displayCategories(datas) {
     const element = document.querySelector('#accordion');
